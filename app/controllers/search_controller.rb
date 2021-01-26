31d8c24cd8da7511277451533d32c3013b6cfca7
Ely Hess <Ely.hess@me.com>
2021-01-25 18:09:11 -0700

    Refactor index action

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,15 +1,8 @@
 class SearchController < ApplicationController
+
 	def index
 		state = params[:state]
-
-		conn = Faraday.new(url: "https://api.propublica.org") do |faraday|
-			faraday.headers["X-API-Key"] = ENV["PROPUBLICA_API_KEY"]
-		end
-
-		response = conn.get("/congress/v1/members/house/#{state}/current.json")
-
-		json = JSON.parse(response.body, symbolize_names: true)
-
-		@members = json[:results]
+		@search = MemberSearchResult.new(state)
 	end
+
 end
