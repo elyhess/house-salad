1167a09eae370cfe95b744dfbf167c9f0d78eeb2
Ely Hess <Ely.hess@me.com>
2021-01-25 18:07:39 -0700

    Add member poro

---
+++ app/POROS/member.rb
@@ -0,0 +1,14 @@
+class Member
+	attr_reader :name,
+	            :role,
+	            :district,
+	            :party
+
+	def initialize(attributes)
+		@name = attributes[:name]
+		@role = attributes[:role]
+		@district = attributes[:district]
+		@party = attributes[:party]
+	end
+
+end
