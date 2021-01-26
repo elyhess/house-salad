270b0b0c9b77279f8c2eec00002558ce0c6ec044
Ely Hess <Ely.hess@me.com>
2021-01-25 18:07:26 -0700

    Add propublica service

---
+++ app/services/propublica_service.rb
@@ -0,0 +1,22 @@
+class PropublicaService
+
+	def initialize(state)
+		@state = state
+	end
+
+	def members_by_state
+		get_json("/congress/v1/members/house/#{@state}/current.json")
+	end
+
+	def conn
+		Faraday.new(url: "https://api.propublica.org") do |faraday|
+			faraday.headers["X-API-Key"] = ENV["PROPUBLICA_API_KEY"]
+		end
+	end
+
+	def get_json(url)
+		response = conn.get(url)
+		data = JSON.parse(response.body, symbolize_names: true)
+	end
+
+end
